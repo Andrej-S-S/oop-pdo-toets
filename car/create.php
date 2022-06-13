2f4e7b70d7663865969812eb68c9b55ab84a81bf
Andrej-S-S <90258552+Andrej-S-S@users.noreply.github.com>
2022-06-13 13:03:04 +0200

    Here in this page you can make a new record

---
+++ car/create.php
@@ -0,0 +1,27 @@
+
+
+<h3>Hier bij kunt een nieuwe record aanmaken!</h3>
+<form action="./create.php" method="post">
+    <table>
+        <tr>
+            <td><label for="model">Model: </label></td>
+            <td><input type="text" name="model" id="model"></td>
+        </tr>
+        <tr>
+            <td><label for="merk">Merk: </label></td>
+            <td><input type="text" name="merk" id="merk"></td>
+        </tr>
+        <tr>
+            <td><label for="topsnelheid">Topsnelheid: </label></td>
+            <td><input type="text" name="topsnelheid" id="topsnelheid"></td>
+        </tr>
+        <tr>
+            <td><label for="prijs">Prijs: </label></td>
+            <td><input type="number" name="prijs" id="prijs"></td>
+        </tr>
+        <tr>
+            <td><input type="submit" value="sumbit"></td>   	    
+        </tr>
+    </table>
+</form>
+
